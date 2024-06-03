48981d1605779fc4362170bc560334566d0144e3
emilycaiano <emilytitacaiano@gmail.com>
2024-06-03 09:26:49 -0300

    Create drive.php

---
+++ drive.php
@@ -0,0 +1,41 @@
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Menu de Multiplas Seleções </title>
+ 
+        <!-- CSS -->
+<link rel="stylesheet" href="css/drives.css">
+
+<body>
+<div class="container-wrapper">
+<div class="container">
+<div class="select-btn">
+<a class="btn-text" href="https://drive.google.com/drive/folders/1pX6Pt4UQ1InMucyY5mGIq5oUPfnvyDQ9?usp=drive_link">Português</a>
+</div>
+</div>
+ 
+        <div class="container">
+<div class="select-btn">
+<a class="btn-text"  href="https://drive.google.com/drive/folders/1q4Yochff49CEY7iYRHQ90bQcqOs6w6Ig?usp=drive_link">Matemática</a>
+</div>
+</div>
+ 
+        <div class="container">
+<div class="select-btn">
+<a class="btn-text" href="https://drive.google.com/drive/folders/11P9cM4rXGIxjfD-qaM67l7W0CV5SO_vD?usp=drive_link">História</a>
+</div>
+</div>
+ 
+        <div class="container">
+<div class="select-btn">
+<a class="btn-text" href="https://drive.google.com/drive/folders/1zya7gnhwZMLSOqB5DKTBrNIcddgG-m01?usp=drive_link">Biologia</a>
+</div>
+</div>
+ 
+        <div class="container">
+<div class="select-btn">
+<a class="btn-text" href="https://drive.google.com/drive/folders/1IHry3TijQPABdJcZ_ZjL6S77wT14xp7p?usp=drive_link">Química</a>
+</div>
+</div>
