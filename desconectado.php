8879cd47b011d1cc6bf3348ffb796f348e5345e4
rairodrs <raissardias387@gmail.com>
2024-06-03 09:23:43 -0300

    Add files via upload

---
+++ desconectado.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de Aguarde</title>
+    <link rel="stylesheet" href="styles/desconectado.css">
+</head>
+<body>
+    <div class="overlay">
+        <div class="loading-box">
+            <div class="container">
+                <div class="content first-content">
+                    <div class="first-column">
+                        <br>
+                        <br>
+                        <br>
+                        <img src="imagens/unistudydesc.png"UniStudy Logo>
+                        <h2>Você foi desconectado!</h2>
+                        <p class="description description-second">Recomendamos que feche a aba</p>
+                        <br>
+                        <br>
+                        <button id="signin" class="btn btn-primary"> <h3>ENTRAR </h3></button>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                    </div>    
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
